026e5c8a2d42cfe1d933387e501deed50fe70d54
janikibichi <janikibichi@gmail.com>
2017-03-17 11:00:12 +0300

    second database

--- microfinanceUSSD.php
+++ microfinanceUSSD.php
@@ -282,6 +282,9 @@ if(!empty($_POST) && !empty($_POST['phoneNumber'])){
 			        	$sql9aa = "INSERT INTO checkout (`status`,`amount`,`phoneNumber`) VALUES('pending','".$amount."','".$phoneNumber."')";
 			        	$db->query($sql9aa); 
 
+			        	$sql9ab = "INSERT INTO account (`phoneNumber`) VALUES('".$phoneNumber."')";
+			        	$db->query($sql9ab); 			        	
+
 				        break;	
 
 					    case "2":
@@ -306,6 +309,9 @@ if(!empty($_POST) && !empty($_POST['phoneNumber'])){
 						//Create pending record in checkout to be cleared by cronjobs
 			        	$sql9aa = "INSERT INTO checkout (`status`,`amount`,`phoneNumber`) VALUES('pending','".$amount."','".$phoneNumber."')";
 			        	$db->query($sql9aa); 
+
+			        	$sql9ab = "INSERT INTO account (`phoneNumber`) VALUES('".$phoneNumber."')";
+			        	$db->query($sql9ab); 			        	
 	       	
 					    break;
 
@@ -331,6 +337,9 @@ if(!empty($_POST) && !empty($_POST['phoneNumber'])){
 						//Create pending record in checkout to be cleared by cronjobs
 			        	$sql9aa = "INSERT INTO checkout (`status`,`amount`,`phoneNumber`) VALUES('pending','".$amount."','".$phoneNumber."')";
 			        	$db->query($sql9aa); 
+
+			        	$sql9ab = "INSERT INTO account (`phoneNumber`) VALUES('".$phoneNumber."')";
+			        	$db->query($sql9ab); 			        	
 
 					    break;
 
